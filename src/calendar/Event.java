64c111c65135157352bdad8a0945221b5336858b
efsmert <samiareski05@gmail.com>
2025-06-02 21:10:24 -0400

    Finally

---
+++ src/calendar/Event.java
@@ -0,0 +1,24 @@
+package calendar;
+
+public class Event {
+  private String subject;
+  private String location;
+  private Date startDate;
+  private Date endDate;
+  private int startTime; //unix time
+  private int endTime;
+  private boolean status;
+
+  public Event(String subject, String location, Date startDate,
+               Date endDate, int startTime, int endTime, boolean status){
+    this.subject = subject;
+    this.location = location;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.status = status;
+  }
+
+
+}
